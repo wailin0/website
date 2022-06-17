bf390cb151f376ca821e520023853d638e6664b3
OMEGA <wailinhtet.dev@gmail.com>
2022-06-17 10:17:10 +0630

    updated static files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var parseString = require('xml2js').parseString;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
